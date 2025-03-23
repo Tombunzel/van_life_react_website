5ea0dff4bfa0dcb2095c59ea35b028ae6eb392a2
Tom Bunzel <bunzel.tom@gmail.com>
2025-03-23 17:35:14 +0100

    use loaders for van data

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,30 +1,18 @@
 import React from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { Link, useLoaderData, useSearchParams } from "react-router-dom"
 import { getVans } from "../../api"
+
+export function loader() {
+    return getVans()
+}
 
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams()
-    const [vans, setVans] = React.useState([])
-    const [loading, setLoading] = React.useState(false)
     const [error, setError] = React.useState(null)
 
     const typeFilter = searchParams.get("type")
 
-    React.useEffect(() => {
-        async function loadVans() {
-            setLoading(true)
-            try {
-                const data = await getVans()
-                setVans(data)
-            } catch (err) {
-                setError(err)
-            } finally {
-                setLoading(false)
-            }
-        }
-
-        loadVans()
-    }, [])
+    const vans = useLoaderData()
 
     const displayedVans = typeFilter
         ? vans.filter(van => van.type === typeFilter)
@@ -58,14 +46,6 @@ export default function Vans() {
             }
             return prevParams
         })
-    }
-
-    if (loading) {
-        return <h1>Loading...</h1>
-    }
-    
-    if (error) {
-        return <h1>There was an error: {error.message}</h1>
     }
 
     return (
