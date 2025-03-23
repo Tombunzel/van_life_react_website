6366bfa46cc4c1d1b65f182fbe2c5df786f85348
Tom Bunzel <bunzel.tom@gmail.com>
2025-03-23 13:16:07 +0100

    adjust vans' Link elements

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -17,14 +17,16 @@ export default function Dashboard() {
 
     function renderVanElements(vans) {
         const hostVansEls = vans.map((van) => (
-            <div className="host-van-single" key={van.id}>
-                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-                <div className="host-van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}/day</p>
+            <Link to={`vans/${van.id}`}>
+                <div className="host-van-single" key={van.id}>
+                    <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                    <div className="host-van-info">
+                        <h3>{van.name}</h3>
+                        <p>${van.price}/day</p>
+                    </div>
+                    <Link to={`vans/${van.id}`}>View</Link>
                 </div>
-                <Link to={`vans/${van.id}`}>View</Link>
-            </div>
+            </Link>
         ))
 
         return (
@@ -33,10 +35,6 @@ export default function Dashboard() {
             </div>
         )
     }
-
-    // if (loading) {
-    //     return <h1>Loading...</h1>
-    // }
 
     if (error) {
         return <h1>Error: {error.message}</h1>
@@ -76,9 +74,6 @@ export default function Dashboard() {
                         </>
                     )
                 }
-                {/*<React.Suspense fallback={<h3>Loading...</h3>}>
-                    <Await resolve={loaderData.vans}>{renderVanElements}</Await>
-                </React.Suspense>*/}
             </section>
         </>
     )
